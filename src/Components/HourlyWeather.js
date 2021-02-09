1245a0a82660e819368d7f27a4bc4aa21ce01395
Tudor Ilisoi <tudorilisoi@yahoo.com>
2021-02-09 21:54:29 +0200

    comments
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- src/Components/HourlyWeather.js
+++ src/Components/HourlyWeather.js
@@ -128,11 +128,13 @@ function HourlyWeather({ hourly }) {
               // Change options for ALL labels of THIS CHART
               datalabels: {
                 formatter: function (value, context) {
+                  // NOTE converting index to string
                   const index = '' + graphData.datasets[0].data.indexOf(+value)
-                  console.log(`value, ${value}`, index)
                   const isMax = minMax.maxlist.includes(index)
                   const isMin = minMax.minlist.includes(index)
                   const symbol = isMax?'▲ ':isMin?'▼ ':''
+
+                  //NOTE converting index back to number
                   if (isMax || isMin || +index === 0 || +index === graphData.datasets[0].data.length - 1) {
                     return `${symbol}${value.toFixed(1)}°`
 
